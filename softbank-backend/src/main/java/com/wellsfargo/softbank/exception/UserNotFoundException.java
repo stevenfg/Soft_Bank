470475c07d9e299f7e78840d61012ad8b8d41de5
stevenfg <52657590+stevenfg@users.noreply.github.com>
2022-11-08 20:11:52 +0530

    Delete softbank-backend directory

--- softbank-backend/src/main/java/com/wellsfargo/softbank/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.wellsfargo.softbank.exception;
-/* Created by Arjun Gautam */
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Could not found the user with id "+ id);
-    }
-}
--- softbank-backend/src/main/java/com/wellsfargo/softbank/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wellsfargo.softbank.repository;
-
-import com.wellsfargo.softbank.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
